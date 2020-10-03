3b283fe4d97de9190774132572da445097fe7a31
Shashank Chandran <iamcrazyshank@gmail.com>
2020-10-03 21:04:24 +0100

    Initial Commit

---
+++ RyanairCodeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RyanairCodeTest
+//
+//  Created by Shashank Chandran on 10/3/20.
+//  Copyright © 2020 Shashank Chandran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
